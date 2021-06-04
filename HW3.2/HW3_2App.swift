6ba6e1909d00c9999d0d1f6528f3b2f399ea7d31
Dima <dmitrysinkov6@gmail.com>
2021-06-04 08:45:39 +0300

    Initial Commit

---
+++ HW3.2/HW3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_2App.swift
+//  HW3.2
+//
+//  Created by Dmitry on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
